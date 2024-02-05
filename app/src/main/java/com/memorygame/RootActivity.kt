56b2f94e80c564c6fcd692e621dd53fcd6467111
OlegWork <xqjh@mail.ru>
2024-02-05 14:05:05 +0300

    Добавлены зависимости для compose Compose Compiler Version 1.4.6
    Создана RootActivity. При запуске на экране отображается текст "HELLO"

--- app/src/main/java/com/memorygame/RootActivity.kt
+++ app/src/main/java/com/memorygame/RootActivity.kt
@@ -1,4 +1,15 @@
 package com.memorygame
 
-class RootActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+
+class RootActivity :ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { 
+            Text(text = "HELLO")
+        }
+    }
 }
